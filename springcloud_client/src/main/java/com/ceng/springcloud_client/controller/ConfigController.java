27e00600dd02d3fb2e8207b4b9e1f0171337304d
cengizhan <cozcan@netas.com.tr>
2018-04-06 17:54:02 +0300

    add oauth2 parameter

--- springcloud_client/src/main/java/com/ceng/springcloud_client/controller/ConfigController.java
+++ springcloud_client/src/main/java/com/ceng/springcloud_client/controller/ConfigController.java
@@ -2,6 +2,7 @@ package com.ceng.springcloud_client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,11 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConfigController {
 
-    @Value("${lucky-word}")
-    private String luckyWord;
+	@Value ("${security.oauth2.clientId}")
+	private String clientId;
 
-    @GetMapping("/lucky-word")
-    public String showLuckyWord() {
-        return "The lucky is: " + luckyWord;
-    }
+	@Value ("${security.oauth2.accessTokenUri}")
+	private String clientAccessTokenUri;
+
+	@Value ("${security.oauth2.clientSecret}")
+	private String clientSecret;
+
+	@RequestMapping ("/oauth2Info")
+	public String getOAuth2Information() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Client Id: ").append(clientId)
+				.append("\nClient Secret: ").append(clientSecret)
+				.append("\nClient Access Token Uri: ").append(clientAccessTokenUri);
+
+		return builder.toString();
+	}
 }
